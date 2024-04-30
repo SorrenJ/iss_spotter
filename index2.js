dc0fa53394f444a717345e6432907786f6b6f43c
SorrenJ <soalexjao@gmail.com>
2024-04-30 01:24:40 -0700

    Promises: fetchCoordsByIP

--- index2.js
+++ index2.js
@@ -1,10 +1,12 @@
 
 const { fetchMyIP } = require('./iss_promised');
+const { fetchCoordsByIP } = require('./iss_promised');
 
 fetchMyIP()
-  .then(body => console.log(body));
+.then(fetchCoordsByIP)
+.then(body => console.log(body));
 
   // Output:
   // {"ip":"10.22.100.000"}
 
-  
+
--- iss_promised.js
+++ iss_promised.js
@@ -10,6 +10,17 @@ const fetchMyIP = function() {
     return request('https://api.ipify.org?format=json');
   };
   
+
+
+
+
+/* 
+ * Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+
+
   const fetchCoordsByIP = function(body) {
     const ip = JSON.parse(body).ip;
     return request(`http://ipwho.is/${ip}`);
